5d1809909b5cdca1bb75a65f962d7673f517b6f2
Kirklin12 <kirkzorns@yahoo.com>
2026-05-19 05:52:37 -0500

    update master m3u playlist

---
+++ epg/scripts/models/guide.ts
@@ -0,0 +1,76 @@
+import { Collection, Logger } from '@freearhey/core'
+import { Storage } from '@freearhey/storage-js'
+import { EPGGrabber } from 'epg-grabber'
+import { Channel, Program } from '.'
+import utc from 'dayjs/plugin/utc'
+import dayjs from 'dayjs'
+import path from 'node:path'
+import pako from 'pako'
+
+dayjs.extend(utc)
+
+interface GuideData {
+  channels: Collection<Channel>
+  programs: Collection<Program>
+  filepath: string
+  gzip: boolean | string
+  json: boolean | string
+}
+
+export class Guide {
+  channels: Collection<Channel>
+  programs: Collection<Program>
+  filepath: string
+  gzip: boolean | string
+  json: boolean | string
+
+  constructor(data: GuideData) {
+    this.channels = data.channels
+    this.programs = data.programs
+    this.filepath = data.filepath
+    this.gzip = data.gzip || false
+    this.json = data.json || false
+  }
+
+  addChannel(channel: Channel) {
+    this.channels.add(channel)
+  }
+
+  toString() {
+    const currDate = dayjs.utc(process.env.CURR_DATE || new Date().toISOString())
+    const headers = { date: currDate.format('YYYYMMDD') }
+
+    return EPGGrabber.generateXMLTV(this.channels.all(), this.programs.all(), headers)
+  }
+
+  async save({ logger }: { logger: Logger }) {
+    const dir = path.dirname(this.filepath)
+    const storage = new Storage(dir)
+    const xmlFilepath = this.filepath
+    const xmlFilename = path.basename(xmlFilepath)
+    logger.info(`  saving to "${xmlFilepath}"...`)
+    const xmltv = this.toString()
+    await storage.save(xmlFilename, xmltv)
+
+    if (this.gzip) {
+      const compressed = pako.gzip(xmltv)
+      const gzFilepath = typeof this.gzip === 'string' ? this.gzip : `${this.filepath}.gz`
+      const gzFilename = path.basename(gzFilepath)
+      logger.info(`  saving to "${gzFilepath}"...`)
+      await storage.save(gzFilename, compressed)
+    }
+
+    if (this.json) {
+      const filename = path.basename(this.filepath).split('.')[0]
+      const jsonFilepath =
+        typeof this.json === 'string' ? this.json : path.join(dir, `${filename}.json`)
+      const currDate = dayjs.utc(process.env.CURR_DATE || new Date().toISOString())
+      const headers = { date: currDate.format('YYYYMMDD') }
+      const channels = this.channels.map((channel: Channel) => channel.toObject()).all()
+      const programs = this.programs.map((program: Program) => program.toObject()).all()
+      const json = JSON.stringify({ ...headers, channels, programs })
+      logger.info(`  saving to "${jsonFilepath}"...`)
+      await storage.save(jsonFilepath, json)
+    }
+  }
+}
---
+++ epg/sites/ziggogo.tv/ziggogo.tv.config.js
@@ -0,0 +1,104 @@
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const doFetch = require('@ntlab/sfetch')
+const uniqBy = require('lodash.uniqby')
+
+dayjs.extend(utc)
+
+module.exports = {
+  site: 'ziggogo.tv',
+  days: 2,
+  request: {
+    cache: {
+      ttl: 24 * 60 * 60 * 1000 // 1 day
+    }
+  },
+  url({ date, segment = 0 }) {
+    return `https://staticqbr-prod-nl.gnp.cloud.ziggogo.tv/eng/web/epg-service-lite/nl/en/events/segments/${date.format(
+      'YYYYMMDD'
+    )}${segment.toString().padStart(2, '0')}0000`
+  },
+  async parser({ content, channel, date }) {
+    const programs = []
+    if (!content) return []
+    const parsed = typeof content === 'string' ? JSON.parse(content) : content
+    if (!Array.isArray(parsed.entries)) return []
+    const entries = parsed.entries
+
+    // fetch other segments
+    let segments = [
+      module.exports.url({ date, segment: 6 }),
+      module.exports.url({ date, segment: 12 }),
+      module.exports.url({ date, segment: 18 })
+    ]
+    await doFetch(segments, (url, res) => {
+      if (Array.isArray(res.entries)) {
+        entries.push(...res.entries)
+      }
+    })
+
+    let events = []
+    entries
+      .filter(item => item.channelId === channel.site_id)
+      .forEach(item => {
+        if (!Array.isArray(item.events)) return
+        events.push(
+          ...item.events.map(event => ({
+            startTime: event.startTime,
+            url: `https://spark-prod-nl.gnp.cloud.ziggogo.tv/eng/web/linear-service/v2/replayEvent/${event.id}?returnLinearContent=true&forceLinearResponse=true&language=nl`
+          }))
+        )
+      })
+
+    events = uniqBy(events, 'startTime')
+
+    // fetch detailed guide
+    if (events.length) {
+      await doFetch(events, (url, res) => {
+        programs.push({
+          title: res.title,
+          subTitle: res.episodeName,
+          description: res.longDescription ? res.longDescription : res.shortDescription,
+          category: res.genres,
+          season: res.seasonNumber,
+          episode: res.episodeNumber,
+          country: res.countryOfOrigin,
+          actor: res.actors,
+          director: res.directors,
+          producer: res.producers,
+          date: res.productionDate,
+          start: dayjs.utc(res.startTime * 1000),
+          stop: dayjs.utc(res.endTime * 1000)
+        })
+      })
+    }
+
+    return programs
+  },
+  async channels() {
+    const channels = []
+    const axios = require('axios')
+    const res = await axios
+      .get(
+        'https://spark-prod-nl.gnp.cloud.ziggogo.tv/eng/web/linear-service/v2/channels?cityId=65535&language=en&productClass=Orion-DASH&platform=web'
+      )
+      .then(r => r.data)
+      .catch(console.error)
+
+    if (Array.isArray(res)) {
+      channels.push(
+        ...res
+          .filter(item => !item.isHidden)
+          .map(item => {
+            return {
+              lang: 'nl',
+              site_id: item.id,
+              name: item.name
+            }
+          })
+      )
+    }
+
+    return channels
+  }
+}
